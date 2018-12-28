71108bdab99d78788254b02d51941406c632f6ca
QLingx <270547728@qq.com>
2018-12-28 16:51:29 +0800

    fix:select number of fds

--- server.c
+++ server.c
@@ -35,6 +35,22 @@ static inline int createSocket(unsigned int port)
     return sock;
 }
 
+static inline void handle(Server *server,int fd,fd_set *activeFDs,struct sockaddr_in *addr){
+    int nread;
+    char buff[20480];
+
+    if((nread = recv(fd,buff,sizeof(buff),0)) < 0){
+        fprintf(stderr,"Failed to call recv\n");
+    }else{
+        buff[nread] = '\0';
+        fprintf(stdout,buff);
+    }
+
+    close(fd);
+
+    FD_CLR(fd,activeFDs);
+}
+
 void serverServe(Server *server)
 {
     int sock = createSocket(server->port);
@@ -72,18 +88,22 @@ void serverServe(Server *server)
                 exit(-1);
             }
 
-            if (newSock > nfds)
+            if (newSock >= nfds)
             {
                 nfds = newSock + 1;
             }
             FD_SET(newSock, &activeFDs);
         }
 
-        for (int fd = sock + 1; fd < nfds; fd++)
+        fprintf(stdout,"-----------------------------------------------here\n");
+        for (int fd = sock + 1; fd < nfds; ++fd)
         {
+            fprintf(stdout,"totalfd:%d,currentfd:%d\n",nfds,fd);
             if (FD_ISSET(fd, &readFDs))
             {
-                fprintf(stdout, "handle socket %d", fd);
+                // fprintf(stdout, "handle socket %d\n", fd);
+                // FD_CLR(fd,&activeFDs);
+                handle(server,fd,&activeFDs,&addr);
             }
         }
     }
